1ae44da1fb65542bba774149f1fd1a9eb6ee851d
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-29 00:31:06 +0300

    basic layout file done

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+            <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800"></div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='text-3xl text-sky-300'>
       Hello World
     </div>
   )
