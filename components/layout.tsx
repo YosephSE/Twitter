7a6eaa56b191d2627866dcccd82d99e6069f1d7d
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-29 00:38:19 +0300

    minor

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
-            <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800"></div>{children}</div>
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800"></div>
+          {children}
+        </div>
       </div>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import Layout from "@/components/Layout";
+import Layout from "@/components/layout";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
