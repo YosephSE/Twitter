78eeb8584abfe179011aa004ab4499669f975c71
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-29 01:12:38 +0300

    sidebar basics done

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Sidebar from "./layout/Sidebar";
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -8,12 +9,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
-          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800"></div>
-          {children}
+            <Sidebar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
         </div>
       </div>
     </div>
-  );
+  ) 
 };
 
 export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
-import Layout from "@/components/layout";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
