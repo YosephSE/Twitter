b1f0c348f7e2f594ae1c4fbf2fe95b17f1c97e3d
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-29 02:50:00 +0300

    Header component done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-3xl text-sky-300'>
-      Hello World
-    </div>
+    <>
+    <Header label='Home' />
+    </>
   )
 }
 
