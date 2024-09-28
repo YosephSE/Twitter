46493c7a3182a16c4f92fe31a85e35080af81d7e
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-29 01:25:26 +0300

    mobile sidebar finished

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-const SidebarItem = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React from "react";
+import { IconType } from "react-icons";
+interface SidebarItemProps {
+  label: string;
+  href: string;
+  icon: IconType;
+  onclick?: () => void;
 }
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  label,
+  href,
+  icon: Icon,
+  onclick,
+}) => {
+  return (
+    <div className="flex flex-grow items-center">
+      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+        <Icon size={28} color="white" />
+      </div>
+    </div>
+  );
+};
 
-export default SidebarItem
+export default SidebarItem;
