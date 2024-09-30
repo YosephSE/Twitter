0b29beb792c5696100627a63e9f700ca99b65212
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-10-01 01:11:10 +0300

    register modal created

--- hooks/useRegisterMoal.ts
+++ hooks/useRegisterMoal.ts
@@ -1,15 +1,15 @@
 import { create } from "zustand";
 
-interface LoginModalState {
+interface registerModalState {
     isOpen: boolean;
     onOpen: () => void;
     onClose: () => void;
 }
 
-const useLoginModal = create<LoginModalState>((set) => ({
+const useRegisterModal = create<registerModalState>((set) => ({
     isOpen: true,
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false }),
 }));
 
-export default useLoginModal;
+export default useRegisterModal;
